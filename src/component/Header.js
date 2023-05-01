92205dfb0a1c606a58deb523eec7c610f51317df
mogfull7777 <anjunyoung94@gmail.com>
2023-05-01 13:42:00 +0900

    recap #3

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import {Col, Container, Nav, Navbar, Row} from "react-bootstrap";
+import {Col, Collapse, Container, Nav, Navbar, Row} from "react-bootstrap";
 
 const Header = () => {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <Navbar bg="light" expand="lg">
-                        <Container fluid>
-                            <Navbar.Brand href="/">J.Y Store</Navbar.Brand>
-                            <Navbar.Toggle aria-controls="navbarScroll" />
-                            <Navbar.Collapse id="navbarScroll" className="justify-content-end">
-                                <Nav className="ml-auto">
-                                    <Nav.Link href="/login">Login</Nav.Link>
-                                    <Nav.Link href="/signup" eventKey={2}>SignUp</Nav.Link>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
-                </Col>
-            </Row>
-        </Container>
+        <Navbar collapseOnSelect expand="lg">
+            <Container>
+                <Navbar.Brand>
+                    JY Store
+                </Navbar.Brand>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className={"ml-auto"}>
+                        <Nav.Link href={"/login"}>Login</Nav.Link>
+                        <Nav.Link href={"/signup"}>Sign Up</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
